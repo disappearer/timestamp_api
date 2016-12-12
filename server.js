c7544a43cc5fbe5397c32908676892b8185ca82a
Aleksa <aleksa47@gmail.com>
2016-12-12 17:49:30 +0000

    Seemingly final version...

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express()
+var dateformat = require('dateformat')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:time', function (req, res) {
+    
+  var dateString = req.params.time;
+  var unixtime = parseInt(dateString)
+  var date = new Date(unixtime)
+  if(isNaN(unixtime)){
+      date = new Date(dateString)
+  }
+  
+  var dateObj = {
+      unix: null,
+      natural: null
+  }
+  
+  if (!isNaN(date.valueOf())) {
+      dateObj.unix = date.valueOf()
+      dateObj.natural = dateformat(date,'mmmm d, yyyy')
+  }
+  
+  res.send(dateObj)
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp app listening on port 8080!')
 })
