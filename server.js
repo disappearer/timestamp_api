f9f4e395373857e788fab8b159c38c2985b21e79
Aleksa <aleksa47@gmail.com>
2016-12-19 18:56:09 +0000

    Switched to environment port

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/:time?', function (req, res) {
   res.send(dateObj)
 })
 
-app.listen(8080, function () {
+var port = process.env.PORT || 8080
+app.listen(port, function () {
   console.log('Timestamp app listening on port 8080!')
 })
